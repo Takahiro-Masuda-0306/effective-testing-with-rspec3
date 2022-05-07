7bb391f450019c77d461010d49c3a0f49fc5cf9e
Takahiro-Masuda-0306 <taka.baseball.0306@gmail.com>
2022-05-07 18:00:52 +0900

    Marking Work in Progress

---
+++ 02-running-specs/09/spec/coffee_spec.rb
@@ -0,0 +1,35 @@
+class Coffee
+  def ingredients
+    @ingredients ||= []
+  end
+
+  def add(ingredient)
+    ingredients << ingredient
+  end
+
+  def price
+    1.00 + ingredients.size * 0.25
+  end
+end
+
+RSpec.configure do |config|
+  config.filter_run_when_matching(focus: true)
+  config.example_status_persistence_file_path = '02-running-specs/logs/rspec_logs.txt'
+end
+
+RSpec.describe 'A cup of coffee' do
+  let(:coffee) { Coffee.new }
+
+  it 'costs $1' do
+    expect(coffee.price). to eq(1.00)
+  end
+
+  context 'with milk', focus: true do
+    before { coffee.add :milk }
+    it 'costs $1.25' do
+      expect(coffee.price).to eq(1.25)
+    end
+    it 'is light in color'
+    it 'is cooler than 200 degrees Fahrenheit'
+  end
+end
