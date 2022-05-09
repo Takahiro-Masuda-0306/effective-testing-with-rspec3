d88a70bac8780417fffeea4e38426f684db3372a
Takahiro-Masuda-0306 <taka.baseball.0306@gmail.com>
2022-05-09 09:12:14 +0900

    Filling In the First Spec

---
+++ 05-unit-specs/03/expence_tracker/app/api.rb
@@ -0,0 +1,18 @@
+require 'sinatra/base'
+require 'json'
+
+module ExpenseTracker
+  class API < Sinatra::Base
+    def initialize(ledger: Ledger.new)
+      @ledger = ledger
+      super()
+    end
+    post '/expenses' do
+      JSON.generate('expense_id'=>32)
+    end
+
+    get '/expenses/:date' do
+      JSON.generate([])
+    end
+  end
+end
